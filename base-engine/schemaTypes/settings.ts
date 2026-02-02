40ec4f085d448dba455afeb0974c37473bf20561
CédricGOUYON <gouyoncedric@gmail.com>
2026-02-02 15:07:55 +0100

    push 6

--- base-engine/schemaTypes/settings.ts
+++ base-engine/schemaTypes/settings.ts
@@ -1,19 +1,24 @@
+import { CogIcon } from "@sanity/icons";
 import { defineField, defineType } from "sanity";
 
 export const settingsType = defineType({
   name: "settings",
-  title: "Configuration",
+  title: "Configuration du Site",
   type: "document",
+  icon: CogIcon,
   fields: [
     defineField({
       name: "siteTitle",
       type: "string",
-      title: "Titre de la boutique",
+      title: "Nom de votre Boutique",
+      description: "Ce nom apparaîtra en haut de votre site.",
     }),
     defineField({
       name: "heroSubtitle",
       type: "text",
-      title: "Texte de bienvenue",
+      title: "Message d'accueil",
+      description:
+        "Le texte principal qui souhaite la bienvenue à vos clients.",
     }),
   ],
 });
